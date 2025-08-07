f9011ff68901b18a9883cb3efcbfd29c1f132785
User <user@example.com>
2025-08-07 19:11:47 -0300

    fix: remover ultimo diretorio de backup

--- backup-2025-07-30_15-48/app/avatar-demo/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AvatarDemo } from "@/components/avatar-demo"
-
-export default function AvatarDemoPage() {
-  return (
-    <div className="container mx-auto p-8">
-      <AvatarDemo />
-    </div>
-  )
-} 
