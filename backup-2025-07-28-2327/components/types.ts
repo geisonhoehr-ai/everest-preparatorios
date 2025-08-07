72898d57ccfdffc6f384cc2c8a0f95db0b5056b9
User <user@example.com>
2025-08-07 19:05:24 -0300

    fix: remover diretório de backup que estava interferindo no build

--- backup-2025-07-28-2327/components/types.ts
+++
@@ -1,6 +0,0 @@
-export interface FlashcardQuantityModalProps {
-  topicName: string;
-  totalFlashcards: number | (() => Promise<number>);
-  onStartStudy: (quantity: number) => void;
-  onClose: () => void;
-} 
